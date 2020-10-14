668d67e22bfdbce03bd6b745b4489a03529560f0
nylander <Johan.Nylander@nrm.se>
2020-10-14 16:08:27 +0200

    increase speed in init.sh

--- workflow/scripts/init.sh
+++ workflow/scripts/init.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # File: init.sh
-# Last modified: mån okt 12, 2020  06:06
+#
+# Last modified: ons okt 14, 2020  04:07
+#
 # Sign: JN
+#
 # Usage: ./init.sh
+#
 # Description: Convert and concatenate fasta, create hmms.
 # To be used with birdscanner2 workflow as an ad-hoc script to
 # avoid running a lot of calls to rules when on a Slurm system.
-# The script basically runs rules OO1_convert_fas, OO2_cat_reference_fas, 
-# OO9_fasta_to_stockholm, and O10_create_hmms.
+# The script basically runs rules OO1_convert_fas, OO2_fasta_to_stockholm,
+# OO3_create_hmms, and OO4_cat_reference_fas -- but without snakemake.
+#
 # Requirements: nucleotide fasta files (suffix .fas) in folder data/references;
 # softwares awk, fasta2stockholm.pl, hmmbuild, and gnuparallel.
+#
 # Copyright (C) 2020 Johan Nylander <johan.nylander@nrm.se>
 # Distributed under terms of the MIT license. 
 
@@ -46,8 +52,10 @@ find data/references -type f -name \*.fas | \
     parallel do_parallel {} "${f2s}"
 
 if [ ! -e run/tmp/reference.fas ] ; then
-    find run/tmp -type f -name \*.fasta -print0 | xargs -0 cat -- >> run/tmp/reference.fas
-    #find run/tmp -type f -name \*.fasta -exec cat {} \+ > run/tmp/reference.fas
+    #find run/tmp -type f -name \*.fasta -print0 | xargs -0 cat -- >> run/tmp/reference.fas
+    ## timing: 1m21.205s
+    find run/tmp -type f -name \*.fasta -exec cat {} \+ > run/tmp/reference.fas
+    ## timing: 0m32.374s
 fi
 
 echo "End of script init.sh"
